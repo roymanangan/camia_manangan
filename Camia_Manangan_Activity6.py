1a6362a35ac9b51cc8654175e48cb64bf2c819e4
roymanangan <rtmanangan@carc.pshs.edu.ph>
2026-09-17 16:07:55 +0800

    Add files via upload

---
+++ Camia_Manangan_Activity6.py
@@ -0,0 +1,40 @@
+#Roy Jose T. Manangan
+#8-Camia
+#09/17/26
+
+#Activity 6
+
+#GRADE CHECKER
+
+#Inputs
+grade = int(input("What is your grade: "))
+
+#Solution
+if 0 <= grade <= 100:
+    print("Valid Grade", grade)
+else:
+    print("Invalid Grade")
+
+#PIN VALIDATOR
+
+#Inputs
+pin = int(input("What is your PIN: "))
+
+#Solution
+if len(str(pin)) == 4:
+    print("Valid PIN", pin)
+else:
+    print("Invalid PIN. Enter 4 digit PIN")
+
+#STUDENT SCORE ENTRY
+
+#Input
+score = int(input("What is your score: "))
+
+#Solution
+if score 0 <= score <= 100:
+print("Valid Score", score)
+
+
+else ValueError:
+    print("Invalid Score, Enter a whole number")
